67c01a8005909e5a09234890e057adea10f0fab4
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-04-02 20:27:22 +0000

    created version two of t1 removed couts

---
+++ T1/v2t1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int size;   // size of the array
+    int op = 0; // number of operations
+    int enter;
+    bool repeating = false; // flag for repetition of a value in the new array comparing to the old
+   
+    cin >> size;    
+    
+    int recipe[size];  // recipe for the dance
+    int aux[size];     // array that is going to be changed
+    int old_aux[size]; // array that is going to store the last modification
+
+    for (int i = 0; i < size; i++)
+    { // loop for filling the array and the recipe
+        cin >> recipe[i];
+        old_aux[i] = recipe[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    { // change based on the recipe
+        aux[i] = old_aux[recipe[i]];
+    }
+    
+    while (!repeating)
+    {
+        bool diff = false;
+        // check if the array is repeating
+        for (int i = 0; i < size; i++)
+        {
+            if (aux[i] != recipe[i])
+            {
+                diff = true;
+                break;
+            }
+        }
+        if (!diff)
+        {
+            repeating = true;
+            break;
+        }
+
+        for (int i = 0; i < size; i++)
+        { // change based on the recipe
+            aux[i] = old_aux[recipe[i]];
+        }
+        op++; // count the operation
+        
+        for (int i = 0; i < size; i++)
+        { // store the last modification in old_aux
+            old_aux[i] = aux[i];
+        }
+    }
+
+    cout << "op: " << op << endl;
+    return 0;
+}
